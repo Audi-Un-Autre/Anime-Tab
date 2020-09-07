e48ffae046cdc7bf53bbf7ad2ac5b31bc0263d28
Audrey Williams <43081195+Audi-Un-Autre@users.noreply.github.com>
2020-09-07 02:00:32 -0400

    Minor refactor, Enums, User Menu & Input, View Database

--- src/database/DataEntry.java
+++ src/database/DataEntry.java
@@ -5,16 +5,99 @@ import java.util.*;
 
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import org.yaml.snakeyaml.*;
 
 public class DataEntry {
 
-    public static void main(String[] args){
-        try{
-            getConnection();
-        } catch(Exception e) {
-            System.out.println(e);
+    // DB operations allowed to user
+    private enum Commands{
+        ADD(1), VIEW(2), QUIT(3);
+        
+        Commands(int option){
+            this.option = option;
         }
+
+        private final int option;
+        public int getOption(){
+            return option;
+        }
+
+        public static Commands getCommandName(int command){
+            for (Commands c : Commands.values()){
+                if (c.getOption() == command){
+                    return c;
+                }
+            }
+            System.out.println("Error: Enum not found.");
+            return null;
+        }
+
+        // Searches through all enums to compare if argument exists in current enum set
+        public static boolean contains(int command){
+            for (Commands c : Commands.values()){
+                if (c.getOption() == command){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    
+    public static void main(String[] args){
+        Commands command = GetInput();
+        
+        // If we want to add to or view the database, establish connection first and then call appropriate database functions
+        switch(command){
+            case ADD:
+                System.out.println("User, you have chosen to add an entry to the database.");
+                break;
+
+            case VIEW:
+                System.out.println("User, you have chosen to view the database.");
+                View();
+                break;
+
+            case QUIT:
+                System.out.println("Exiting . . .");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Something has gone wrong.");
+                break;
+        }
+    }
+
+    private static Commands GetInput(){
+        Scanner in = new Scanner(System.in);
+        int input = 0;
+        Commands command = null;
+
+        // Main Menu
+        System.out.println("Welcome to Anime Tab.");
+        System.out.println("\n Anime Tab! Menu:\n");
+        System.out.print("1: Add Entry\n2: View All Entries\n3: Quit.\n\nEnter an option . . .");
+
+        // Get valid option input from user
+        if (in.hasNextInt()){
+            input = in.nextInt();
+            if (Commands.contains(input)){
+                command = Commands.getCommandName(input);
+            } else {
+                do{
+                    System.out.println("Enter a valid option . . . : ");
+                    if (in.hasNextInt()){
+                        input = in.nextInt();
+                        if (Commands.contains(input)){
+                            command = Commands.getCommandName(input);
+                        }
+                    }
+                }while(!Commands.contains(input));
+            }
+        }
+        in.close();
+        return command;
     }
 
     private static Auth GetAuth() throws Exception{
@@ -56,6 +139,23 @@ public class DataEntry {
 
         return null;
     }
+
+    private static void View(){
+        Connection connection;
+        try{
+            connection = getConnection();
+            PreparedStatement statement = connection.prepareStatement("SELECT title FROM work");
+
+            ResultSet results = statement.executeQuery();
+            //ArrayList<String> array = new ArrayList<String>();
+            while(results.next()){
+                System.out.println(results.getString("title"));
+            }
+            connection.close();
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
 }
 
 // Class to hold auth data
@@ -66,5 +166,55 @@ final class Auth{
     public Auth(String un, String pw){
         this.un = un;
         this.pw = pw;
+    }
+}
+
+final class InsertData{
+    int id, year;
+    String title, titleAlias, author, authorAlias, workType, language;
+    File image;
+
+    public InsertData(){}
+    public InsertData(String title, String titleAlias, String author, String authorAlias, int year, String workType, String language, File image){
+        this.title = title;
+        this.titleAlias = titleAlias;
+        this.author = author;
+        this.authorAlias = authorAlias;
+        this.year = year;
+        this.workType = workType;
+        this.language = language;
+        this.image = image;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setTitleAlias(String titleAlias){
+        this.titleAlias = titleAlias;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void setAuthorAlias(String authorAlias){
+        this.authorAlias = authorAlias;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setWorkType(String workType){
+        this.workType = workType;
+    }
+
+    public void setLanguage(String language){
+        this.language = language;
+    }
+
+    public void setImage(File image){
+        this.image = image;
     }
 }
